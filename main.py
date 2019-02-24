f99ffce44c1c9a187f09789c721597df8bee0a47
Stephen Lu <stephen.lu.2002@gmail.com>
2019-02-24 15:07:27 -0500

    -Different pages and class tables

--- main.py
+++ main.py
@@ -17,11 +17,66 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 db = SQLAlchemy(app)		# database
 
-#Home Page
+class Users(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(80), unique=True, nullable=False)
+	password = db.Column(db.String(80), nullable=False)
+	userType = db.Column(db.String(40), nullable=False)
+
+	def __repr__(self):
+		return f"User('{self.username}', '{self.userType}')"
+
+class Judges(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	judgeName = db.Column(db.String(80), unique=True, nullable=False)
+	totalMoney = db.Column(db.Integer)
+
+	def __repr__(self):
+		return f"Judge('{self.judgeName}', '{self.totalMoney}')"
+
+class Teams(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	teamName = db.Column(db.String(80), unique=True, nullable=False)
+	teamDescription = db.Column(db.String(500), nullable=False)
+	teamPhoto = db.Column(db.String(20), nullable=False, default='team_photo.jpg')
+	totalPoints = db.Column(db.Integer)
+
+	def __repr__(self):
+		return f"Team('{self.teamName}', '{self.totalPoints}')" 
+
+#Login Page
 @app.route('/', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	error = None
+	form_username = request.get
+	form_password = request.get
+	return render_template('login.html', error=error)
+
+
+#Register New User
+
+@app.route('/register', methods=['GET'])
+def chooseType():
+	return render_template('chooseType.html')
+
+@app.route('/register/team', methods=['GET', 'POST'])
+def newTeam():
+	return render_template('teamForm.html')
+
+@app.route('/register/judge', methods=['GET', 'POST'])
+def newJudge():
+	return render_template('judgeForm.html')
+
+
+
+#User Pages
+
 @app.route('/home', methods=['GET', 'POST'])
 def home():
 	return render_template("home.html")
+
+
 
 
 if __name__ == '__main__':
