f77c1d299cf7c91a65fd5802fb316edc4d1acf64
Stephen Lu <stephen.lu.2002@gmail.com>
2019-02-24 15:45:21 -0500

    Add webpage paths and html templates

--- main.py
+++ main.py
@@ -18,63 +18,84 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 db = SQLAlchemy(app)		# database
 
 class Users(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	username = db.Column(db.String(80), unique=True, nullable=False)
-	password = db.Column(db.String(80), nullable=False)
-	userType = db.Column(db.String(40), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    userType = db.Column(db.String(40), nullable=False)
 
-	def __repr__(self):
-		return f"User('{self.username}', '{self.userType}')"
+    def __repr__(self):
+        return f"User('{self.username}', '{self.userType}')"
 
 class Judges(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	judgeName = db.Column(db.String(80), unique=True, nullable=False)
-	totalMoney = db.Column(db.Integer)
+    id = db.Column(db.Integer, primary_key=True)
+    judgeName = db.Column(db.String(80), unique=True, nullable=False)
+    totalMoney = db.Column(db.Integer)
 
-	def __repr__(self):
-		return f"Judge('{self.judgeName}', '{self.totalMoney}')"
+    def __repr__(self):
+        return f"Judge('{self.judgeName}', '{self.totalMoney}')"
 
 class Teams(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	teamName = db.Column(db.String(80), unique=True, nullable=False)
-	teamDescription = db.Column(db.String(500), nullable=False)
-	teamPhoto = db.Column(db.String(20), nullable=False, default='team_photo.jpg')
-	totalPoints = db.Column(db.Integer)
+    id = db.Column(db.Integer, primary_key=True)
+    teamName = db.Column(db.String(80), unique=True, nullable=False)
+    teamDescription = db.Column(db.String(500), nullable=False)
+    teamPhoto = db.Column(db.String(20), nullable=False, default='team_photo.jpg')
+    totalPoints = db.Column(db.Integer)
 
-	def __repr__(self):
-		return f"Team('{self.teamName}', '{self.totalPoints}')" 
+    def __repr__(self):
+        return f"Team('{self.teamName}', '{self.totalPoints}')" 
 
 #Login Page
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-	error = None
-	form_username = request.get
-	form_password = request.get
-	return render_template('login.html', error=error)
+    error = None
+
+    if request.method == 'POST':
+        form_username = request.form['username']
+        form_password = request.form['password']
+        real_username = Users.query.filter_by(username=username).first()
+
+        if real_username:
+            if real_username.username != form_username: error = 'Invalid credentials. Please try again!'
+            else:
+                real_password = real_username.password
+                if real_password == form_password:
+                    #Define Account Data and Parameters
+                    return render_template('home.html')
+
+        else: error = 'Invalid credentials. Please try again!'
+
+    return render_template('login.html', error=error)
+
 
 
 #Register New User
 
 @app.route('/register', methods=['GET'])
 def chooseType():
-	return render_template('chooseType.html')
+    return render_template('chooseType.html')
 
 @app.route('/register/team', methods=['GET', 'POST'])
 def newTeam():
-	return render_template('teamForm.html')
+    if request.method == 'POST':
+        teamName = 
+    return render_template('teamForm.html')
 
 @app.route('/register/judge', methods=['GET', 'POST'])
 def newJudge():
-	return render_template('judgeForm.html')
+    return render_template('judgeForm.html')
 
 
 
 #User Pages
 
-@app.route('/home', methods=['GET', 'POST'])
-def home():
-	return render_template("home.html")
+@app.route('/judgeHome', methods=['GET', 'POST'])
+def Jhome():
+    return render_template("judgeHome.html")
+
+@app.route('/teamHome', methods=['GET', 'POST'])
+def Thome():
+    return render_template("teamHome.html")
 
 
 
